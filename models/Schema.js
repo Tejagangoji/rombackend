c41ab2e27f60a96ed9c78f4839499075c1abead5
teja <tejagangoji@gmail.com>
2024-07-04 14:37:20 +0530

    last

--- models/Schema.js
+++ models/Schema.js
@@ -4,12 +4,29 @@ const userSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true },
     password: { type: String },
-    orders: {
-        type: Array,
-        default: []
-    }
+});
+
+const cartSchema = new mongoose.Schema({
+    userid: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    productid: { type: mongoose.Schema.Types.ObjectId, ref: "Product" },
+});
+
+const orderSchema = new mongoose.Schema({
+    userid: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    products: { type: Array, required: true },
+    total: { type: String, require: true }
+});
+
+const productSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    image: { type: String, required: true },
+    price: { type: String, required: true },
+    category: { type: String, required: true },
 });
 
 const User = mongoose.model("User", userSchema);
+const Product = mongoose.model("Product", productSchema);
+const Cart = mongoose.model("Cart", cartSchema);
+const Order = mongoose.model("Order", orderSchema);
 
-module.exports = { User };
+module.exports = { User, Product, Cart, Order };
--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require('cors');
-const {User} = require("./models/Schema");
+const { User, Product, Cart, Order } = require("./models/Schema");
 const app = express();
 
 mongoose.connect("mongodb+srv://user:user@cluster0.hzfuazk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0").then(() => console.log("db is running"));
@@ -9,10 +9,11 @@ mongoose.connect("mongodb+srv://user:user@cluster0.hzfuazk.mongodb.net/?retryWri
 app.use(cors());
 app.use(express.json());
 
-app.post('/register', async(req, res) => {
-    const {name, email, password} = req.body;
-    const alredyemail = await User.findOne({email: email});
-    if(alredyemail) {
+//user registration
+app.post('/register', async (req, res) => {
+    const { name, email, password } = req.body;
+    const alredyemail = await User.findOne({ email: email });
+    if (alredyemail) {
         return res.status(400).json("User is already there");
     }
     const user = User({
@@ -24,29 +25,132 @@ app.post('/register', async(req, res) => {
     return res.status(200).json("Register sucessfully");
 })
 
-app.post('/login', async(req, res) => {
-    const {email, password} = req.body;
-    const alredyemail = await User.findOne({email: email});
-    if(!alredyemail) {
+//user login
+app.post('/login', async (req, res) => {
+    const { email, password } = req.body;
+    const alredyemail = await User.findOne({ email: email });
+    if (!alredyemail) {
         return res.status(404).json("User no found");
     }
-    if(alredyemail.password === password) {
-        return res.status(200).json("login sucessfully");
+    if (alredyemail.password === password) {
+        return res.status(200).json(alredyemail.id);
     }
     return res.status(405).json("password is incorrect");
 })
 
+//add to cart
+app.post('/addtocart', async (req, res) => {
+    try {
+        const { userid, productid } = req.body;
+        const cart = Cart({
+            userid,
+            productid
+        });
+        await cart.save();
+        return res.status(200).json("added in to the cart");
+    } catch (error) {
+        return res.status(500).json("server error");
+    }
+})
+
+//remove from cart
+app.delete('/removefromcart/:id/:userid', async (req, res) => {
+    try {
+        await Cart.deleteOne({ userid: req.params.userid, productid: req.params.id });
+        return res.status(200).json("removed sucessful")
+    } catch (error) {
+        return res.status(500).json("server error");
+    }
+})
+
+//get the cart
+app.get('/getthecart/:userid', async (req, res) => {
+    try {
+        const cart = await Cart.find({ userid: req.params.userid });
+        return res.status(200).json(cart);
+    } catch (error) {
+        return res.status(500).json("server error");
+    }
+});
+
+//get the cart
+app.get('/getthecartwith/:userid', async (req, res) => {
+    try {
+        const cart = await Cart.find({ userid: req.params.userid }).populate("productid");
+        return res.status(200).json(cart);
+    } catch (error) {
+        return res.status(500).json("server error");
+    }
+})
+
+//place an order
+app.post('/placeanorder', async (req, res) => {
+    try {
+        const { userid, products, total } = req.body;
+        const order = Order({
+            userid,
+            products,
+            total
+        });
+        await order.save()
+        return res.status(200).json("order is successful")
+    } catch (error) {
+        return res.status(500).json("server error");
+    }
+})
+
+//get the order details
+app.get('/getorders', async(req, res) => {
+    return res.status(200).json(await Order.find().populate('userid'))
+})
 
 
 
-//admin
-
-app.post('/adminlogin', async(req, res) => {
-    const {email, password} = req.body;
-    if(email === "admin@gmail.com" && password === "Admin123") {
+//admin login
+app.post('/adminlogin', async (req, res) => {
+    const { email, password } = req.body;
+    if (email === "admin@gmail.com" && password === "Admin123") {
         return res.status(200).json("admin login successful");
     }
     return res.status(405).json("credentials are incorrect");
+});
+
+//add a product
+app.post('/addproduct', async (req, res) => {
+    try {
+        const { name, image, price, category } = req.body;
+        const product = Product({
+            name,
+            image,
+            price,
+            category
+        });
+        await product.save()
+        return res.status(200).json("add succesfylly");
+    } catch (error) {
+        return res.status(500).json("server error");
+    }
+});
+
+//get the products
+app.get('/getproducts', async (req, res) => {
+    try {
+        return res.status(200).json(await Product.find());
+    } catch (error) {
+        return res.status(500).json("server error");
+
+    }
+});
+
+//delete the product
+app.delete('/deleteproduct/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        await Product.findByIdAndDelete(id);
+        return res.status(200).json("Deleted successfully")
+    } catch (error) {
+        return res.status(500).json("server error");
+    }
 })
 
 app.listen(5000, () => console.log("server is running"));
